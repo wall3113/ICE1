cbb6e377496449196a2738da1b9ae2538395a14e
Wallace Ip <ipch@sheridancollege.ca>
2023-01-28 14:54:31 -0500

    I'm Done

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -66,6 +66,7 @@ public class CardTrick {
      * replace this information with your own.
      * @author CHI YIN IP Jan 28 2023
      */
+    //I'm done!
     private static void printInfo() {
     
         System.out.println("Congratulations, you guessed right!");
