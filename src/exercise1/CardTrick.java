3c615df6733cbef43ee7bfac93464356954ca655
CHI YIN IP <119081867+wall3113@users.noreply.github.com>
2023-01-28 14:35:27 -0500

    Update personal info in GitHub

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -71,19 +71,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is CHI YIN, but you can call me Wallace");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Be a programmer in Big Tech");
+        System.out.println("-- Use what I have learnt in college to solve problems in daily life");
+	System.out.println("-- Be able to write different trading strategies");
 	System.out.println();	
 
         System.out.println("My hobbies:");
         System.out.println("-- Investing");
         System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Watching movies");
 
         System.out.println();
 }}
